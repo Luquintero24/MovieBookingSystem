de55735e6a3882a991cb31c129f2ea622209c3db
Luisa Quintero <lmqr2004@gmail.com>
2025-04-14 17:44:11 -0500

    firebase-setup

---
+++ js/create-account.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7UQTcpoKETfZZT2LZ0AT7mh_jaSZthGA",
+  authDomain: "moviebooking-20705.firebaseapp.com",
+  projectId: "moviebooking-20705",
+  storageBucket: "moviebooking-20705.firebasestorage.app",
+  messagingSenderId: "230927628782",
+  appId: "1:230927628782:web:5460063cce3d8d55e8f6ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
